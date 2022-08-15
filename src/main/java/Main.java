17c8a941e5cf599621b49ca9faa4ef3c0e41c38e
DanRBJ <d.r.bond.juez@hotmail.com>
2022-08-15 21:51:42 +0200

    Main with launch in no package

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
-package org.example;
-
 import core.FileCipherDecipher;
 import javafx.application.Application;
+import model.FileCipher;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Application.launch(FileCipherDecipher.class, args);
+
+
+
+
     }
 }
