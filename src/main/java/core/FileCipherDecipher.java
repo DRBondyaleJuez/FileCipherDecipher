7a4a3d8a2a16af3c16dd803a1453bd68064196cc
DanRBJ <d.r.bond.juez@hotmail.com>
2022-08-15 19:08:44 +0200

    Main created and core created with launch and loadFXML declared

---
+++ src/main/java/core/FileCipherDecipher.java
@@ -0,0 +1,51 @@
+package core;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class FileCipherDecipher extends Application {
+
+    private Stage mainStage;
+
+    public FileCipherDecipher() { mainStage = new Stage();}
+
+    @Override public void start(Stage stage)  throws Exception {
+        mainStage = stage;
+        loadingMainScene();
+
+        mainStage.setTitle("File-Cipher-Decipher");
+        mainStage.centerOnScreen();
+        mainStage.show();
+    }
+
+    private void loadingMainScene() {
+        // TO access to the Resource folder, you have to do the following:
+        // getClass().getResource("/path/of/the/resource")
+        FXMLLoader paneLoader = new FXMLLoader(getClass().getResource("/view/MainView.fxml"));
+        Parent root = loadPaneLoader(paneLoader);
+        Scene newScene = new Scene(root);
+        mainStage.setScene(newScene);
+
+    }
+
+    private Parent loadPaneLoader(FXMLLoader paneLoader) {
+        try {
+            return paneLoader.load();
+        } catch (IOException e) {
+            //Todo: log!!
+            //Todo do something if the try fails
+            System.out.println("FAIL!!! EXPLOTION!!!! BOOOOOOM");
+            System.out.println(e);
+            return null;
+        }
+    }
+
+
+
+
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import core.FileCipherDecipher;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(FileCipherDecipher.class, args);
     }
 }
