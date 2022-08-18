0395a64d3360d7e19752e99db6c7a24b4d2143bf
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-08-18 11:16:03 +0200

    Minor changes. Editing out comments

--- src/main/java/model/FileDivider.java
+++ src/main/java/model/FileDivider.java
@@ -30,12 +30,6 @@ public class FileDivider implements Runnable{
     public FileDivider() {
     }
 
-    //Perhaps id attribute
-    //directory attribute
-    //The idea is to give each divider a number and depending on that number and they will work on particular
-    // sections of the file and multiples of that particular starting section soy they don't step on each other
-
-    //Don't forget the builder
 
     @Override
     public void run() {
