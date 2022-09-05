f54873e30de932526dc496acf4007860063d3380
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-09-06 00:29:17 +0200

    Fixed the issue regarding the fileoutputstream overwriting instead of appending. this was solve adding the attribute true when calling the method. However since now append to avoid issues when a file is deciphered twice lines of code were added where first the old file is removed before making the new deciphering

--- src/main/java/utils/deciphering/FileJoiner.java
+++ src/main/java/utils/deciphering/FileJoiner.java
@@ -2,6 +2,7 @@ package utils.deciphering;
 
 import model.FileDecipherDeposit;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -18,9 +19,18 @@ public class FileJoiner implements Runnable {
     @Override
     public void run() {
 
+        //Check if file deciphered already exists
+        File possiblePreviousDecipherFile = new File(decipherFileStorage);
+        if(possiblePreviousDecipherFile.exists()){
+            boolean deletedPreviousSimilarDecipher = possiblePreviousDecipherFile.delete();
+            if(deletedPreviousSimilarDecipher) {
+                System.out.println("A previous deciphering of file - " + decipherFileStorage + " - has been eliminated before the new deciphering"); //////////////////DELETE WHEN FINISHED
+            }
+        }
+
+
         for (int i = 0; i < fileDecipherDeposit.viewTotalNumberOfParts(); i++) {
             byte[] byteArrayForFiling = fileDecipherDeposit.getBytesReadyToFile(i);
-
             storeDecipherFile(byteArrayForFiling);
             System.out.println("Deciphered part " + i + " stored in file " + decipherFileStorage); ///////////////////////DELETE WHEN FINISHED
         }
@@ -31,7 +41,7 @@ public class FileJoiner implements Runnable {
 
         FileOutputStream fos = null;
         try {
-            fos = new FileOutputStream(decipherFileStorage); /////UNABLE TO FIX THIS WARNING
+            fos = new FileOutputStream(decipherFileStorage,true); /////UNABLE TO FIX THIS WARNING
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Unable to find a file to store cipher bytes: " + decipherFileStorage);
