cc07ba1c932c88d93e4e749eaf3b88d1ae2aef59
Daniel RBJ <d.r.bond.juez@hotmail.com>
2022-09-06 00:38:17 +0200

    To allow for the deciphering of bigger files the removal of certain arrays during filejoining was added and described and the addition of a timed wait to prevent to many arrays of bytes to be added which lead to an OutOfMemoryError. So the file deciphers encounter a sort of buffer when adding decipher byte arrays if the file joiner has not caught up with filing of byte array and hence eliminating them from the filedecipherdeposit.

--- src/main/java/model/FileDecipherDeposit.java
+++ src/main/java/model/FileDecipherDeposit.java
@@ -8,10 +8,13 @@ public class FileDecipherDeposit {
     private int numberOfPartsDeciphered;
     private final ArrayList<Byte>[] bytesReadyToFileArray;
 
+    private int numberOfPartsAddedToFile;
+
     public FileDecipherDeposit(int partsToBeDeciphered) {
         totalNumberOfParts = partsToBeDeciphered;
         numberOfPartsDeciphered = 0;
         bytesReadyToFileArray = new ArrayList[partsToBeDeciphered]; /////////////////////////DON'T KNOW HOW TO FIX THIS WARNING
+        numberOfPartsAddedToFile = 0;
     }
 
     public synchronized int getPartToDecipher(){
@@ -30,7 +33,7 @@ public class FileDecipherDeposit {
 
     public synchronized byte[] getBytesReadyToFile(int part){
 
-        while(bytesReadyToFileArray[part].isEmpty()){
+        while(bytesReadyToFileArray[part]==null){
             try {
                 wait();
             } catch (InterruptedException e) {
@@ -42,12 +45,30 @@ public class FileDecipherDeposit {
         for (int i = 0; i < bytesReadyToFileArray[part].size(); i++) {
             bytesToFile[i] = bytesReadyToFileArray[part].get(i);
         }
-        bytesReadyToFileArray[part].clear();
+
+        //After the byteArray is extracted and ready to return to the filejoiner the arraylist source is cleared from
+        //the file decipher deposit to free space to allow deciphering of bigger files.
+        bytesReadyToFileArray[part] = null;
+        numberOfPartsAddedToFile++;
+        notifyAll();
 
         return bytesToFile;
     }
 
     public synchronized void addBytesToFile(int part,ArrayList<Byte> listOfDecipheredBytes){
+
+        //To try to avoid out of memory error we buffered slightly the addition of byte arrays by the threads here
+        while(part > numberOfPartsAddedToFile + 10){
+            try {
+                System.out.println("I am a deciphering thread trying to add part " + part + " to decipher deposit. But there have been only " + numberOfPartsAddedToFile +
+                        " parts added to the file. To avoid out of memory error I will wait 2 seconds");
+                wait(2000);
+            } catch (InterruptedException e) {
+                System.out.println("The deciphering of part " +part+" was interrupted.");
+                throw new RuntimeException(e);
+            }
+        }
+
         bytesReadyToFileArray[part] = listOfDecipheredBytes;
         notifyAll();
     }
